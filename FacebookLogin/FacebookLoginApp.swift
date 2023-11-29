cf5f04239516f15a679368e4a9851c2ea39a098d
Pedro Eduardo Vilarinho Andrade <pedrovilarinho.gpd@gmail.com>
2023-11-29 14:21:32 -0300

    Initial Commit

---
+++ FacebookLogin/FacebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookLoginApp.swift
+//  FacebookLogin
+//
+//  Created by Pedro Andrade on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FacebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
